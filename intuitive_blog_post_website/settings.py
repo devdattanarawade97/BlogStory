5bf332ea09bf033de2b625b6d174305033fb5ca8
devdatta <devdattanarawade97@gmail.com>
2025-01-08 12:58:54 +0530

    django debug env false

--- intuitive_blog_post_website/settings.py
+++ intuitive_blog_post_website/settings.py
@@ -31,13 +31,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DJANGO_DEBUG",True)
+DEBUG = os.environ.get("DJANGO_DEBUG")
 
 if DEBUG:
 
     ALLOWED_HOSTS = []
     SECRET_KEY=os.environ.get('DJANGO_SECRET_KEY')
-    
+
 else:
     ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 
